7d46dd2aa6eea1466484b43ce08cd284c8fdd9f1
Emil Sehested <emilsted@gmail.com>
2016-04-12 15:53:00 +0200

    yo

---
+++ newmvc/newmvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="newmvc.MvcApplication" Language="C#" %>
---
+++ newmvc/newmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(newmvc.Startup))]
+namespace newmvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
